ce3c7f1065c1d95f82ade9ab8d6d427768d5257f
LifesGoood <dragan.velinov@students.finki.ukim.mk>
2023-11-08 13:22:28 +0100

    tweaks

--- src/CsvExporter.java
+++ src/CsvExporter.java
@@ -5,15 +5,12 @@ import java.util.List;
 public class CsvExporter {
     public static void saveDataToCsvFile(List<WineryInfo> wineryData, String fileName) {
         try (FileWriter fileWriter = new FileWriter(fileName)) {
-            fileWriter.append("Name,Address,Rating,Location\n");
+            fileWriter.append("Name;Address;Rating;Location\n");
             for (WineryInfo info : wineryData) {
-                String address = info.address.replace(",", "-");
-                String location = info.location.toString().replace(",", "-");
-
-                fileWriter.append(info.name).append(",");
-                fileWriter.append(address).append(",");
-                fileWriter.append(String.valueOf(info.rating)).append(",");
-                fileWriter.append(location).append("\n");
+                fileWriter.write(info.name + ";");
+                fileWriter.write(info.address + ";");
+                fileWriter.write(info.rating + ";");
+                fileWriter.write(info.location + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
