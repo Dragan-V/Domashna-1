4ea95b1f71aa849d7c7fc584e19ed9090ac44e34
LifesGoood <dragan.velinov10@gmail.com>
2023-11-10 19:14:35 +0100

    expanded search

--- src/WineryDataExporter.java
+++ src/WineryDataExporter.java
@@ -20,9 +20,18 @@ public class WineryDataExporter {
                 new LatLng(41.52297, 22.35999),
                 new LatLng(41.93242, 21.69960),
                 new LatLng(42.14813, 22.27066),
-                new LatLng(41.83265, 20.95572)
+                new LatLng(41.83265, 20.95572),
+                new LatLng(41.45005,22.22156),
+                new LatLng(41.75242,22.55919),
+                new LatLng(42.11101,22.18597),
+                new LatLng(41.81062,21.90612),
+                new LatLng( 41.53209,21.58333),
+                new LatLng( 41.07832,20.96742),
+                new LatLng( 41.69503,20.89885),
+                new LatLng( 42.05295,21.33640),
+                new LatLng(41.09672,21.60100)
         };
-        int[] radii = { 50000, 50000, 40000, 20000, 30000 };
+        int radius = 50000;
 
         List<WineryInfo> allWineryData = new ArrayList<>();
 
@@ -30,7 +39,7 @@ public class WineryDataExporter {
             try {
                 TextSearchRequest request = PlacesApi.textSearchQuery(context, "wineries")
                         .location(locations[i])
-                        .radius(radii[i]);
+                        .radius(radius);
                 PlacesSearchResponse response = request.await();
                 for (PlacesSearchResult result : response.results) {
                   //  System.out.println(result.rating);
